3cd850c9e60ac9c7e5a4b0e04f8354fa37e378bb
Eddie Davis <eddied@vulcan.com>
2020-12-10 15:36:20 -0800

    Add fv3core dependencies

--- install.sh
+++ install.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-version=sn_1.0
+version=vcm_1.0
 env_file=env.daint.sh
 dst_dir=/project/s1053/install/venv/${version}
 
 # versions
 fv3config_sha1=2212b33a2ec8e2e05df10e1c9ca0f1815d4f9a8d
-gt4py_url="git+git://github.com/GridTools/gt4py.git"
-gt4py_sha1=204838ea0e88a30b6eb41babbd58fb8a86d14a0e
+gt4py_url="git+git://github.com/VulcanClimateModeling/gt4py.git"
 cuda_version=cuda102
 
 # module environment
@@ -31,6 +30,12 @@ pip install numpy
 pip install matplotlib
 pip install cupy-${cuda_version}
 
+# installation of fv3 dependencies
+pip install cftime xarray zarr f90nml
+
+# build and install mpi4py
+MPICC=cc python -m pip install --upgrade git+https://github.com/mpi4py/mpi4py.git
+
 # installation of our packages
 pip install git+git://github.com/VulcanClimateModeling/fv3config.git@${fv3config_sha1}
 pip install ${gt4py_url}#egg=gt4py[${cuda_version}]
@@ -41,14 +46,14 @@ deactivate
 cat >> ${dst_dir}/bin/activate <<EOF1
 
 # echo module environment
-echo "Note: this virtual env has been created on `hostname` using the folowing module environment:"
-cat <<'EOF'
-EOF1
-cat ./env/${env_file} >> ${dst_dir}/bin/activate
-cat >> ${dst_dir}/bin/activate <<EOF2
-EOF
+# echo "Note: this virtual env has been created on `hostname` using the folowing module environment:"
+# cat <<'EOF'
+# EOF1
+# cat ./env/${env_file} >> ${dst_dir}/bin/activate
+# cat >> ${dst_dir}/bin/activate <<EOF2
+# EOF
 
-EOF2
+# EOF2
 
 # done
 exit 0
