9eef77861ec2dd2b299f7096f9635b7437b7c2fa
eddie-c-davis <22508135+eddie-c-davis@users.noreply.github.com>
2020-12-15 11:27:31 -0700

    Add fv3core dependencies and build mpi4py from source (#2)
    * Add fv3core dependencies
    * Add more packages
    * Build mpi4py from source
    * Remove trailing cat
    * Add 'hypothesis'
    * Add activate cat back
    * Remove 'mpi4py' directory from previous install
    * Remove unused entries in mpi.cfg

--- install.sh
+++ install.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-version=sn_1.0
+version=vcm_1.0
 env_file=env.daint.sh
 dst_dir=/project/s1053/install/venv/${version}
 
 # versions
 fv3config_sha1=2212b33a2ec8e2e05df10e1c9ca0f1815d4f9a8d
-gt4py_url="git+git://github.com/GridTools/gt4py.git"
-gt4py_sha1=204838ea0e88a30b6eb41babbd58fb8a86d14a0e
+gt4py_url="git+git://github.com/VulcanClimateModeling/gt4py.git"
 cuda_version=cuda102
 
 # module environment
@@ -30,6 +29,19 @@ pip install --upgrade wheel
 pip install numpy
 pip install matplotlib
 pip install cupy-${cuda_version}
+pip install pytest pytest-profiling pytest-subtests hypothesis
+
+# installation of fv3 dependencies
+pip install cftime f90nml pyyaml xarray zarr
+
+# build and install mpi4py from sources
+rm -rf ./mpi4py
+export MPICC=cc
+git clone https://github.com/mpi4py/mpi4py.git
+cp ./mpi.cfg ./mpi4py
+cd mpi4py
+python setup.py build --mpi=mpi
+python setup.py install
 
 # installation of our packages
 pip install git+git://github.com/VulcanClimateModeling/fv3config.git@${fv3config_sha1}
@@ -37,18 +49,6 @@ pip install ${gt4py_url}#egg=gt4py[${cuda_version}]
 python -m gt4py.gt_src_manager install
 deactivate
 
-# add note when activated
-cat >> ${dst_dir}/bin/activate <<EOF1
-
 # echo module environment
-echo "Note: this virtual env has been created on `hostname` using the folowing module environment:"
-cat <<'EOF'
-EOF1
+echo "Note: this virtual env has been created on `hostname`."
 cat ./env/${env_file} >> ${dst_dir}/bin/activate
-cat >> ${dst_dir}/bin/activate <<EOF2
-EOF
-
-EOF2
-
-# done
-exit 0
