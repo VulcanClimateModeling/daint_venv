db684e4ca97d0b979ef0ef180e9e69eac30ffb02
eddie-c-davis <22508135+eddie-c-davis@users.noreply.github.com>
2021-01-08 14:04:25 -0700

    Add more python dependencies (#3)
    * Add fv3core dependencies
    * Add more packages
    * Build mpi4py from source
    * Remove trailing cat
    * Add 'hypothesis'
    * Add activate cat back
    * Remove 'mpi4py' directory from previous install
    * Remove unused entries in mpi.cfg
    * Add more pip dependencies
    * Replace ./ with
    * Prepend environment to venv activate script
    * Unset MPICC variable after installing mpi4py
    
    Co-authored-by: Oliver Fuhrer <ofuhrer@users.noreply.github.com>

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 version=vcm_1.0
 env_file=env.daint.sh
 dst_dir=/project/s1053/install/venv/${version}
+src_dir=$(pwd)
 
 # versions
 fv3config_sha1=2212b33a2ec8e2e05df10e1c9ca0f1815d4f9a8d
@@ -10,9 +11,9 @@ gt4py_url="git+git://github.com/VulcanClimateModeling/gt4py.git"
 cuda_version=cuda102
 
 # module environment
-source ./env.sh
-source ./env/machineEnvironment.sh
-source ./env/${env_file}
+source ${src_dir}/env.sh
+source ${src_dir}/env/machineEnvironment.sh
+source ${src_dir}/env/${env_file}
 
 # echo commands and stop on error
 set -e
@@ -22,36 +23,39 @@ set -x
 if [ -d ${dst_dir} ] ; then /bin/rm -rf ${dst_dir} ; fi
 python3 -m venv ${dst_dir}
 source ${dst_dir}/bin/activate
-pip install --upgrade pip
-pip install --upgrade wheel
+python3 -m pip install --upgrade pip
+python3 -m pip install --upgrade wheel
 
 # installation of standard packages
-pip install numpy
-pip install matplotlib
-pip install cupy-${cuda_version}
-pip install pytest pytest-profiling pytest-subtests hypothesis
+python3 -m pip install kiwisolver numpy matplotlib cupy-${cuda_version} Cython h5py six zipp
+python3 -m pip install pytest pytest-profiling pytest-subtests hypothesis
 
 # installation of fv3 dependencies
-pip install cftime f90nml pyyaml xarray zarr
+python3 -m pip install cftime f90nml pandas pyparsing python-dateutil pytz pyyaml xarray zarr
 
 # build and install mpi4py from sources
-rm -rf ./mpi4py
+rm -rf ${src_dir}/mpi4py
 export MPICC=cc
 git clone https://github.com/mpi4py/mpi4py.git
-cp ./mpi.cfg ./mpi4py
-cd mpi4py
-python setup.py build --mpi=mpi
-python setup.py install
+cp ${src_dir}/mpi.cfg ${src_dir}/mpi4py
+cd mpi4py/
+python3 setup.py build --mpi=mpi
+python3 setup.py install
 cd ../
+unset MPICC
 
 # installation of our packages
-pip install git+git://github.com/VulcanClimateModeling/fv3config.git@${fv3config_sha1}
-pip install ${gt4py_url}#egg=gt4py[${cuda_version}]
-python -m gt4py.gt_src_manager install
+python3 -m pip install git+git://github.com/VulcanClimateModeling/fv3config.git@${fv3config_sha1}
+python3 -m pip install ${gt4py_url}#egg=gt4py[${cuda_version}]
+python3 -m gt4py.gt_src_manager install
 
 # deactivate virtual environment
 deactivate
 
 # echo module environment
 echo "Note: this virtual env has been created on `hostname`."
-cat ./env/${env_file} >> ${dst_dir}/bin/activate
+cat ${src_dir}/env/${env_file} ${dst_dir}/bin/activate > ${dst_dir}/bin/activate~
+mv ${dst_dir}/bin/activate~ ${dst_dir}/bin/activate
+
+exit 0
+
