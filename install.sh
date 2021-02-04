5a465bcaa197721b6bd62b68baeb4423ad5c8e8c
Johann Dahm <johannd@vulcan.com>
2021-02-04 14:40:42 -0800

    Use stable gt4py version (#8)
    * Use stable gt4py version

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 version=vcm_1.0
 env_file=env.daint.sh
@@ -7,8 +7,8 @@ src_dir=$(pwd)
 
 # versions
 fv3config_sha1=1eb1f2898e9965ed7b32970bed83e64e074a7630
-gt4py_url="git+git://github.com/VulcanClimateModeling/gt4py.git"
 cuda_version=cuda102
+# gt4py checks out the latest stable tag below
 
 # module environment
 source ${src_dir}/env.sh
@@ -40,7 +40,7 @@ python3 -m pip install cftime f90nml pandas pyparsing python-dateutil pytz pyyam
 # build and install mpi4py from sources
 rm -rf ${src_dir}/mpi4py
 export MPICC=cc
-git clone https://github.com/mpi4py/mpi4py.git
+git clone --depth 1 https://github.com/mpi4py/mpi4py.git
 cp ${src_dir}/mpi.cfg ${src_dir}/mpi4py
 cd mpi4py/
 python3 setup.py build --mpi=mpi
@@ -48,9 +48,13 @@ python3 setup.py install
 cd ../
 unset MPICC
 
-# installation of our packages
+# installation of fv3config
 python3 -m pip install git+git://github.com/VulcanClimateModeling/fv3config.git@${fv3config_sha1}
-python3 -m pip install ${gt4py_url}#egg=gt4py[${cuda_version}]
+
+# installation of gt4py
+git clone --depth 1 git://github.com/VulcanClimateModeling/gt4py.git gt4py
+(cd gt4py/ && git checkout $(git describe --abbrev=0 --tags))
+python3 -m pip install "gt4py/[${cuda_version}]"
 python3 -m gt4py.gt_src_manager install
 
 # deactivate virtual environment
@@ -62,4 +66,3 @@ cat ${src_dir}/env/${env_file} ${dst_dir}/bin/activate > ${dst_dir}/bin/activate
 mv ${dst_dir}/bin/activate~ ${dst_dir}/bin/activate
 
 exit 0
-
