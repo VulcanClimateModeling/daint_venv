e1ee78ede98da7a47f8175dea396e7c1470b77e2
Tobias Wicky <wicky.tob@gmail.com>
2021-11-02 03:53:11 +0100

    We are now getting: (#25)
    We are now getting:
    "The unauthenticated git protocol on port 9418 is no longer supported."
    This fixes that

--- install.sh
+++ install.sh
@@ -40,10 +40,10 @@ python3 -m pip install --find-links=$wheeldir cupy Cython clang-format
 
 # installation of gt4py
 rm -rf gt4py
-git clone git://github.com/VulcanClimateModeling/gt4py.git gt4py
+git clone git@github.com:ai2cm/gt4py.git gt4py
 cd gt4py
 if [ -z "${GT4PY_VERSION}" ]; then
-    wget 'https://raw.githubusercontent.com/VulcanClimateModeling/fv3core/master/GT4PY_VERSION.txt'
+    wget 'https://raw.githubusercontent.com/ai2cm/fv3core/master/GT4PY_VERSION.txt'
     GT4PY_VERSION=`cat GT4PY_VERSION.txt`
 fi
 git checkout ${GT4PY_VERSION}
